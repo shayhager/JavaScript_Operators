6862f0b54a7a707815dd0a83a9d7257cf39ee8e9
Shayla Hager <shayhager22@hotmail.com>
2022-12-15 09:49:15 -0700

    firstCommit

--- app.js
+++ app.js
@@ -4,16 +4,75 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+undefined
+let b = 4
+undefined
+let sum = a + b;
+undefined
+sum
+24
+console.log(sum)
+let minus = a - b;
+undefined
+minus
+16
+console.log(minus)
+let multiply = a * b;
+undefined
+let dividing = a / b;
+undefined
+multiply
+80
+console.log(multiply)
+dividing
+5
+console.log(dividing)
+let remainder = a % b;
+undefined
+
+
 
 // YOUR CODE HERE
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+
 
 // YOUR CODE HERE
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val == str3)
+console.log(val === str3)
+console.log(!isAwake)
+console.log("eleven" == str4 && val >= str3)
+console.log(!isAwake || isAwake)
+console.log(0 == false)
+console.log(0 === false)
+console.log(0 != false)
+console.log(0 !== false)
+
+
+
 
 // YOUR CODE HERE
